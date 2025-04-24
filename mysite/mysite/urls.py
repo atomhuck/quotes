97dfc3c3a4b435a6a152e9536abbafa6ae9382c1
atomhuck <142097453+atomhuck@users.noreply.github.com>
2025-04-24 19:26:31 +0300

    Заполнена бд

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('quotes/', include('quotes.urls')),
+    path('', include('quotes.urls')),
     path('admin/', admin.site.urls),
 ]
--- mysite/quotes/views.py
+++ mysite/quotes/views.py
@@ -1,26 +1,11 @@
 from django.http import HttpResponse
-import json
+from django.shortcuts import get_object_or_404, render
 from .models import Faculties, Professors, Quotes, QuotesProfessors
-import os
-def index(request):
-    with open('quotes/quotes.json', 'r', encoding='utf-8') as file:
-        data = json.load(file)
 
-    # Запишем данные в таблицу Faculties
-    for entry in data:
-        for entry in data:
-            try:
-                professor = Professors.objects.get(pk=entry['professor_id'])
-            except Professors.DoesNotExist:
-                print(f"Пропущена цитата с professor_id={entry['professor_id']} — такого профессора нет.")
-                continue
-        Quotes.objects.create(
-            text=entry['text'],
-            professor=professor,
-            likes_count=entry['likes_count'],
-            reposts_count=entry['reposts_count'],
-            date=entry['date']
-        )
+def main(request):
+    quotes = Quotes.objects.all()[:9]
+    return render(request, 'quotes/some.html', {'quotes': quotes})
 
-    # print("Данные успешно загружены!")
-    return HttpResponse(f"Current working directory: {os.getcwd()}")
+def facs(request):
+    faculties = Faculties.objects.all()[:20]
+    return render(request, 'quotes/faculties.html', {'faculties': faculties})
